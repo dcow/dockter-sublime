a980ef97097566a56a8ebc03f8edf10d92db7bea
David Cowden <david.cowden@lookout.com>
2014-12-09 15:18:30 -0800

    Add remove workspace task

--- buildSrc/src/main/groovy/io/dcow/Sublime.groovy
+++ buildSrc/src/main/groovy/io/dcow/Sublime.groovy
@@ -3,11 +3,27 @@ package io.dcow
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+import org.gradle.api.tasks.Delete
+
 class Sublime implements Plugin<Project> {
     // Provided for name access to the task..
-    public static final String SUBLIME_TASK = 'sublime'
+    public static final String TASK_NAME = 'sublime'
+    public static final String PROJECT_TASK_NAME = 'sublimeProject'
+    public static final String CLEAR_WORKSPACE_TASK_NAME = 'clearWorkspace'
+
+    static final String WORKSPACE_FILE_EXTENSION = 'sublime-workspace'
 
     void apply(Project project) {
-        project.tasks.create(name: SUBLIME_TASK, type: SublimeProject)
+        def tasks = project.tasks
+        tasks.create (name: PROJECT_TASK_NAME, type: SublimeProject)
+        tasks.create (name: CLEAR_WORKSPACE_TASK_NAME, type: Delete) {
+                description "Remove the '$WORKSPACE_FILE_EXTENSION' file."
+                delete "${project.name}.$WORKSPACE_FILE_EXTENSION"
+                shouldRunAfter tasks.sublimeProject
+        }
+        tasks.create (name: TASK_NAME) {
+                description "Generate a project file and clear the workspace cache."
+                dependsOn tasks.sublimeProject, tasks.clearWorkspace
+        }
     }
 }
